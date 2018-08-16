6dece85cba48e54c1c4014fcbc5739c1acc38dfa
Dani Biró <danibiro93@gmail.com>
2018-08-16 11:10:37 +0300

    Fix broken build.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -57,8 +57,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: config.build.productionSourceMap
-        ? { safe: true, map: { inline: false } }
-        : { safe: true }
+        ? { map: { inline: false } }
+        : { }
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
