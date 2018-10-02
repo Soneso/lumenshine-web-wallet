1f5f65c8a4bded9e25df49793c6364393ad2491b
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-02 16:03:47 +0300

    split layout regions; styles the generic footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,32 +1,49 @@
 <template>
-  <footer>
+  <footer class="py-5 text-center">
+    <b-row class="py-2">
+      <b-col>
+        <router-link to="/" class="language-button">
+          <i class="icon-flags"/>
+        </router-link>
+      </b-col>
+    </b-row>
+    <b-row class="py-2 footer-menu">
+      <b-col>
+        <!-- No user was logged in -->
+        <div v-if="!authToken" class="menu">
+          <router-link to="/about">About</router-link>
+          <router-link to="/help">Help</router-link>
+          <router-link to="/login">Login</router-link>
+          <router-link to="/register">Sign up</router-link>
+          <router-link to="/impressum">Impressum</router-link>
+          <a href="https://soneso.com" target="_blank">Soneso</a>
+        </div>
 
-    <!-- No user was logged in -->
-    <div v-if="!authToken" class="menu">
-      <router-link to="/about">About</router-link>
-      <router-link to="/help">Help</router-link>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/register">Sign up</router-link>
-      <router-link to="/impressum">Impressum</router-link>
-      <a href="https://soneso.com" target="_blank">Soneso</a>
-    </div>
+        <!-- Partial user was logged in (without all registration steps completed) -->
+        <div v-if="authToken && authTokenType === 'partial'" class="menu">
+          <router-link to="/about">About</router-link>
+          <router-link to="/help">Help</router-link>
+          <router-link to="/">&nbsp;</router-link>
+          <a href="/login" @click="onLogoutClick">Sign out</a>
+          <router-link to="/impressum">Impressum</router-link>
+          <a href="https://soneso.com" target="_blank">Soneso</a>
+        </div>
 
-    <!-- Partial user was logged in (without all registration steps completed) -->
-    <div v-if="authToken && authTokenType === 'partial'" class="menu">
-      <router-link to="/about">About</router-link>
-      <router-link to="/help">Help</router-link>
-      <router-link to="/">&nbsp;</router-link>
-      <a href="/login" @click="onLogoutClick">Sign out</a>
-      <router-link to="/impressum">Impressum</router-link>
-      <a href="https://soneso.com" target="_blank">Soneso</a>
-    </div>
+        <!-- Logged in user -->
+        <div v-if="authToken && authTokenType !== 'partial'" class="menu menu--short">
+          <a href="https://soneso.com" target="_blank">Developed by Soneso</a>
+          <router-link to="/impressum">Impressum</router-link>
+          <a href="#"><i class="fa fa-flag"/></a>
+        </div>
+      </b-col>
+    </b-row>
 
-    <!-- Logged in user -->
-    <div v-if="authToken && authTokenType !== 'partial'" class="menu menu--short">
-      <a href="https://soneso.com" target="_blank">Developed by Soneso</a>
-      <router-link to="/impressum">Impressum</router-link>
-      <a href="#"><i class="fa fa-flag"/></a>
-    </div>
+    <b-row class="py-2 soneso-logo">
+      <b-col class="text-uppercase">
+        <p>Powered by</p>
+        <img src="../assets/images/ui/soneso-logo.svg">
+      </b-col>
+    </b-row>
 
   </footer>
 </template>
