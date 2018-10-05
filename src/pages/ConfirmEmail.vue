e28e5cce3586f86e5a446f37358e9ef6a805c6c7
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-05 10:25:12 +0300

    confim email

--- src/pages/ConfirmEmail.vue
+++ src/pages/ConfirmEmail.vue
@@ -1,40 +1,44 @@
 <template>
-  <div class="page-box form">
-    <div v-if="!hasToken">
-      <h1>Setup Wallet</h1>
-      <h3>Email confirmation required</h3>
-      <p>In order to be able continue the setup process, you need to confirm your email address first. A mail with a confirmation link has been sent to your inbox. Please follow the instructions from the received mail to confirm your email-address.</p>
-      <div v-if="inProgress">Loading...</div>
-      <div v-else>
-        <div>
-          <div v-if="alreadyConfirmedFailed" class="error">Please confirm your email address first.</div>
-          <button @click="onCheckConfirmation">Continue</button>
-        </div>
-
-        <div v-if="emailResent && resendEmailStatus.err.length > 0" class="error">An error happened during email resend</div>
-        <div v-else-if="emailResent" class="info">Resent confirmation mail</div>
-        <button @click="onResendEmail">Resend confirmation mail</button>
-      </div>
-    </div>
-    <div v-else>
-      <h1>Email Confirmation</h1>
-      <p v-if="confirmEmailStatus.loading">
-        Confirming email address. Loading...
-      </p>
-      <div v-if="!confirmEmailStatus.loading">
-        <div v-if="confirmEmailStatus.err.length > 0" class="error">
-          <div v-for="err in confirmEmailStatus.err" :key="err.error_code">Error: {{ err.error_message }}</div>
-          <div v-if="confirmEmailStatus.err.find(err => err.error_code === 1000)">Invalid confirmation URL!</div>
-          <div v-if="confirmEmailStatus.err.find(err => err.error_code === 1006)">Token expired!</div>
-          <div v-if="confirmEmailStatus.err.find(err => err.error_code === 1008)">Email already confirmed!</div>
-        </div>
-        <div v-else>
-          <p>Thank you for confirming your email address.</p>
-          <button @click="onLoginClick">Login</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <b-row align-h="center" align-v="center">
+    <b-col cols="11" sm="9" md="7" lg="6" xl="5">
+      <b-card class="p-4 text-center">
+        <template v-if="hasToken">
+          <h4 class="form-headline text-uppercase pl-2">Setup Wallet</h4>
+          <h6 class="text-danger py-3">Email confirmation required</h6>
+          <p>In order to be able continue the setup process, you need to confirm your email address first. A mail with a confirmation link has been sent to your inbox. Please follow the instructions from the received mail to confirm your email address.</p>
+          <div v-if="inProgress">Loading...</div>
+          <div v-else>
+            <div class="py-3">
+              <b-button type="submit" variant="info" class="btn-rounded text-uppercase" @click="onCheckConfirmation">Continue</b-button>
+              <div v-if="alreadyConfirmedFailed" class="text-danger">Please confirm your email address first.</div>
+            </div>
+            <br>
+            <b-button type="submit" variant="primary" class="btn-rounded text-uppercase" @click="onResendEmail">Resend confirmation email</b-button>
+            <div v-if="emailResent && resendEmailStatus.err.length > 0" class="text-danger pt-2">An error happened during email resend</div>
+            <div v-else-if="emailResent" class="text-success pt-2">Resent confirmation mail</div>
+          </div>
+        </template>
+        <template v-else>
+          <h4 class="form-headline text-uppercase pl-2">Email Confirmation</h4>
+          <p v-if="confirmEmailStatus.loading" class="text-info py-5">
+            Confirming email address. Loading...
+          </p>
+          <div v-else="" class="pt-4 pb-1">
+            <div v-if="confirmEmailStatus.err.length > 0" class="text-danger">
+              <div v-for="err in confirmEmailStatus.err" :key="err.error_code" class="pb-2">Error: {{ err.error_message }}</div>
+              <div v-if="confirmEmailStatus.err.find(err => err.error_code === 1000)" class="pb-2">Invalid confirmation URL!</div>
+              <div v-if="confirmEmailStatus.err.find(err => err.error_code === 1006)" class="pb-2">Token expired!</div>
+              <div v-if="confirmEmailStatus.err.find(err => err.error_code === 1008)" class="pb-2">Email already confirmed!</div>
+            </div>
+            <div v-else>
+              <p class="text-success pb-4">Thank you for confirming your email address.</p>
+              <b-button type="submit" variant="info" class="btn-rounded text-uppercase" @click="onLoginClick">Login</b-button>
+            </div>
+          </div>
+        </template>
+      </b-card>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -92,6 +96,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-</style>
