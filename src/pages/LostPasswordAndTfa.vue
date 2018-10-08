71b368989ce92feaaf2bd97779534ca405d9d413
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-08 18:04:54 +0300

    lost pass and 2fa secret

--- src/pages/LostPasswordAndTfa.vue
+++ src/pages/LostPasswordAndTfa.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="page-box form">
-    <h1>Lost Password & 2FA Secret</h1>
-    <p>We can not recover your account automatically, please contact</p>
-    <p><a :href="`mailto:${config.SUPPORT_MAIL}?subject=${encodeURIComponent('Lost Password & 2FA Secret')}`">support</a></p>
-  </div>
+  <b-row align-h="center" align-v="center">
+    <b-col cols="11" sm="8" md="6" lg="5" xl="4">
+      <b-card class="p-4 single-card text-center">
+        <h4 class="form-headline pb-3">Lost Password & 2FA Secret</h4>
+        <p>We can not recover your account automatically, please contact</p>
+        <p><a :href="`mailto:${config.SUPPORT_MAIL}?subject=${encodeURIComponent('Lost Password & 2FA Secret')}`">support</a></p>
+      </b-card>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
