6b848c42cd8c2a5dd452949e3407d48d631cf5ac
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-08 18:08:52 +0300

    mnemonic quiz card width wasn't limited

--- src/pages/MnemonicQuiz.vue
+++ src/pages/MnemonicQuiz.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row align-h="center" align-v="center">
     <b-col cols="11" sm="9" md="7" lg="6" xl="5">
-      <b-card class="p-4 text-center">
+      <b-card class="p-4 single-card text-center">
         <h4 class="form-headline text-uppercase pl-2">Setup Wallet</h4>
         <small class="text-secondary">Step 3 of 3</small>
         <h6 class="text-danger pt-3">Prove noting of mnemonic</h6>
