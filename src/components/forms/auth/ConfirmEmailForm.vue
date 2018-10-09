f0863565923b4fc666f330bf2db1c133b74f7f4a
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-09 10:23:52 +0300

    lost 2fa secret step1

--- src/components/forms/auth/ConfirmEmailForm.vue
+++ src/components/forms/auth/ConfirmEmailForm.vue
@@ -5,9 +5,9 @@
       <div v-if="inProgress" class="py-4 px-2">Loading...</div>
       <template v-else>
         <b-button variant="info" class="btn-rounded text-uppercase" @click.prevent="onCheckConfirmation">Already confirmed</b-button>
-        <div v-if="alreadyConfirmedFailed" class="text-danger pt-2 pb-3">Please confirm your email address first.</div>
-        <b-button variant="warning" class="btn-rounded text-uppercase" @click.prevent="onResendEmail">Resend confirmation mail</b-button>
-        <div v-if="emailResent" class="text-success pt-2 pb-3">Resent confirmation mail</div>
+        <small v-if="alreadyConfirmedFailed" class="d-block text-danger py-2">Please confirm your email address first.</small>
+        <b-button variant="warning" class="btn-rounded text-uppercase mt-4" @click.prevent="onResendEmail">Resend confirmation mail</b-button>
+        <small v-if="emailResent" class="d-block text-success py-2">Resent confirmation mail</small>
       </template>
     </template>
   </b-form>
