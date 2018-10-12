ac8384a016e43b923453a399b7feb91502ab6d42
Dani Biró <danibiro93@gmail.com>
2018-10-12 10:16:31 +0300

    Loading label fix

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,7 +2,7 @@
   <b-row align-h="center" align-v="center">
     <b-col cols="8" sm="12" md="8" lg="8" xl="8">
       <b-container fluid>
-        <div v-if="wallets.loading" class="py-4 px-2">Loading...</div>
+        <h5 v-if="wallets.loading" class="py-4 px-2 text-info" style="display: block;"><span class="py-3 px-4" style="display: inline-block; text-align: center; background: rgba(255, 255, 255, 0.5); border-radius: 20px">Loading...</span></h5> <!-- TODO -->
         <b-row align-h="start" align-v="center">
           <wallet-card v-for="wallet in homescreenWallets" :key="wallet.public_key_0" :data="wallet"/>
         </b-row>
