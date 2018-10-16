228ba7abe6367ca32d0b6830cbd45d16da5593c9
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-16 18:55:15 +0300

    animated spinner

---
+++ src/components/ui/spinner2.vue
@@ -0,0 +1,7 @@
+<template>
+  <span class="spinner-wrapper">
+    <i class="icon-spinner2 spin cw">
+      <span class="path1"/><span class="path2"/><span class="path3"/><span class="path4"/><span class="path5"/><span class="path6"/><span class="path7"/><span class="path8"/><span class="path9"/><span class="path10"/><span class="path11"/>
+    </i>
+  </span>
+</template>
