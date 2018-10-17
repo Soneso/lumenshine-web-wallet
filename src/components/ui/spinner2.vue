2d90aba15aaf3969b2d19ee5637a513e3474578a
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-17 15:10:11 +0300

    spinner updates to host messages as well

--- src/components/ui/spinner2.vue
+++ src/components/ui/spinner2.vue
@@ -1,7 +1,39 @@
 <template>
-  <span class="spinner-wrapper">
-    <i class="icon-spinner2 spin cw">
-      <span class="path1"/><span class="path2"/><span class="path3"/><span class="path4"/><span class="path5"/><span class="path6"/><span class="path7"/><span class="path8"/><span class="path9"/><span class="path10"/><span class="path11"/>
-    </i>
-  </span>
+  <div :style="spinnerWidth" class="spinner-wrapper">
+    <div>
+      <i :class="['icon-spinner2 spin cw', color]">
+        <span class="path1"/><span class="path2"/><span class="path3"/><span class="path4"/><span class="path5"/><span class="path6"/><span class="path7"/><span class="path8"/><span class="path9"/><span class="path10"/><span class="path11"/>
+      </i>
+    </div>
+    <div v-if="message">
+      <small :class="color">{{ message }}</small>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Spinner2',
+  props: {
+    width: {
+      type: Number,
+      required: false
+    },
+    color: {
+      type: String,
+      required: false
+    },
+    message: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    spinnerWidth () {
+      return {
+        width: this.message && !this.width ? '200px' : this.width + 'px'
+      };
+    }
+  }
+};
+</script>
