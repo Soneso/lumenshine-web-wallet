248b5b255dccd8a72a75c18f4deaabbcc055d3d7
Cristian Maghiar <cristi@avangard-media.ro>
2018-10-19 14:18:55 +0300

    copy to clipboard component better control

--- src/components/ui/copyToClipboard.vue
+++ src/components/ui/copyToClipboard.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-    <a v-clipboard:copy="text" v-b-tooltip="{title: message, delay: {show: 500, hide: 1000}, target: 'click'}">
+    <a v-clipboard:copy="text" v-b-tooltip="message" :id="`tooltip-${now}`" @click.prevent="showTooltip">
       <i :class="['icon-copy', color]"/>
     </a>
   </span>
@@ -23,6 +23,20 @@ export default {
       type: String,
       required: false,
       default: 'text-info'
+    }
+  },
+  data () {
+    return {
+      now: new Date().getTime()
+    };
+  },
+  mounted () {
+    this.$root.$emit('bv::disable::tooltip', `tooltip-${this.now}`);
+  },
+  methods: {
+    showTooltip () {
+      this.$root.$emit('bv::enable::tooltip', `tooltip-${this.now}`);
+      this.$root.$emit('bv::show::tooltip', `tooltip-${this.now}`);
     }
   }
 };
