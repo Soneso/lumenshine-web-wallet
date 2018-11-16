c75d78de787e4d96f0cd0f3542e553e71fd8aceb
Cristian Maghiar <cristi@avangard-media.ro>
2018-11-16 16:49:30 +0200

    wallet guidelines page

---
+++ src/pages/WalletGuidelines.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-row align-h="center" align-v="center">
+    <b-col sm="10" md="8">
+      <b-card class="p-2 p-md-4 text-justify">
+        <h1 class="pt-3">Wallet Guidelines</h1>
+        <small>Last updated: November 15, 2018</small>
+        <br>
+        <br>
+        <p>When you sign up for Lumenshine, you agree to our terms of service, which explain in detail the requirements and conditions for using the site or mobile apps. Make sure you read and understand those terms of service: these guidelines aren't meant to replace them, just to give you a concise overview of some key concepts to keep in mind as you use Lumenshine.</p>
+        <p>If you follow these guidelines, you will enjoy everything Lumenshine has to offer. If you don't, you may take on more risk than you intend or get locked out of your account.</p>
+
+        <h2 class="pt-4">Understand the risks involved</h2>
+        <p>Making transactions in a blockchain like the Stellar network is inherently risky: transactions are irreversible, and there is no backstop to protect you from loss. If you lack experience or have low risk tolerance, we advise against using Lumenshine and against submitting transactions (such as payments) to the Stellar network.</p>
+
+        <h2 class="pt-4">Be careful with your keys</h2>
+        <p>Lumenshine doesn't hold your assets or keep any of your stellar secret keys or mnemonic. If you don't know your mnemonic (backup secret), and you forget your password, you will be permanently locked out of your account. If that happens, there's nothing we can do to help you regain access.</p>
+        <p>In addition, because transactions are immediate and irreversible, if someone else gains access to your account, they can instantly withdraw all of your assets. Never share your stellar secret keys, password and mnemonic (backup secret) with anyone, including Lumenshine. We'll never ask you for it, and anyone who does is trying to scam you.</p>
+        <p>If you activate fingerprint or face recognition on your mobile device for the Lumenshine app, you must understand that your password will be saved by the app in a secure location on your device (such as in the Keychain on iOS devices). The app can then access your password to log you in automatically or to sign transactions for you when you provide your fingerprint / face. If you enable face or fingerprint recognition in your mobile app, we can not guarantee for the security of your password. It is protected by your device, not by us. You do this on your own risk. </p>
+        <p>The Lumenshine Site and apps never send your password to our server. The app uses cryptographic methods to verify to the server that you have provided the correct password to be able to log you in. You can read more about this kind of authentication method here: <a href="https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0010.md">https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0010.md</a>. </p>
+
+        <h2 class="pt-4">Do your research</h2>
+        <p>Lumenshine doesn't vet or vouch for assets. We are an interface for the publicly accessible Stellar network, where anyone can issue a token. All the information we may display about an asset is provided by the issuer, so it may be incomplete, or even inaccurate.</p>
+        <p>Before you buy or transfer an asset to your wallet, make sure you understand what you're buying or transfering. Read up on a token on the issuer's website, look at the issuer's Stellar account, and study the relevant white paper. If you're interested in an anchored token, verify the reserves of the underlying asset and check the issuer's site for specific conditions, fees, and minimums for deposit and withdrawal.</p>
+        <p>Lumenshine gives you access to all assets provided in the public Stellar network, but it's up to you to ensure that you have sufficient knowledge and experience to use them.</p>
+
+        <h2 class="pt-4">If you have questions or need help, contact us</h2>
+        <p>You can search for answers to common questions 24/7 on our help center. If you don't find what you're looking for there, feel free to email us. We want to make sure you have the best experience possible, and if something comes up, we're happy to help.</p>
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
