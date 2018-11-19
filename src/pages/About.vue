5eb38aaf932244b91313ad3572b433bb4954d301
Cristian Maghiar <cristi@avangard-media.ro>
2018-11-19 20:01:48 +0200

    added about, impressum and help pages

---
+++ src/pages/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-row align-h="center" align-v="center">
+    <b-col sm="10" md="8">
+      <h2>about</h2>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'About'
+};
+</script>
