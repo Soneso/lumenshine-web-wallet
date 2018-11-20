98d2d33b62d655f62bb92c83014d62b093a7bd5d
Christian Rogobete <c.rogobete@soneso.com>
2018-11-20 16:06:21 +0100

    update text on homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,8 +4,8 @@
       <b-card class="p-4">
         <b-row>
           <b-col cols="12" sm="6" md="5" lg="4" xlg="3">
-            <h1><small class="text-info">Introducing</small> <span class="font-weight-800 text-primary">Lumenshine</span></h1>
-            <p class="lead">Lumenshine is a Wallet for the public Stellar network. Lumenshine is developed and maintained by Soneso GmbH, Germany. The Stellar network is hosted by numerous distributed nodes world-wide who are not under the control of any single entity or groups of entities. Lumenshine does not control the Stellar network.</p>
+            <h1><small class="text-info">Introducing</small> <span class="font-weight-800 text-primary">Lumenshine (beta)</span></h1>
+            <p class="lead">Lumenshine is a beautiful, full-featured Wallet for the public Stellar network. It is developed and maintained by Soneso GmbH, Germany. Sign-Up and interact with the Stellar network with ease.</p>
           </b-col>
           <b-col>
             <img src="/static/app-screenshot.png">
@@ -15,12 +15,24 @@
             <b-button href="/login" size="lg" variant="outline-info" class="btn-rounded text-uppercase mx-3">log in</b-button>
           </b-col>
           <b-col cols="12" class="pt-5 mt-5">
-            <h2 class="text-info">All assets, peer-to-peer</h2>
-            <p class="lead">StellarX is built on Stellar’s universal order book. Trades happen directly between traders. No middlemen, no miners. You trade from your own wallet and keep total control of your keys.</p>
+            <h2 class="text-info">Full featured and beautiful</h2>
+            <p class="lead">With Lumenshine you can manage your Stellar accounts, send and receive payments, add an manage your assets from the Stellar network. Add short Stellar federation addresses to your accounts, set inflation destination for airdrops, mange contacts, search, filter and view your transactions, receive push notifications on your iPhone and much more. Lumenshine is beautiful, userfreindly and focuesd on security.</p>
           </b-col>
           <b-col cols="12" class="pt-5 my-5">
-            <h3 class="text-info">Fast and actually free</h3>
-            <p>Transactions settle in seconds, and trading on StellarX costs nothing. We refund your network fees and we even return Stellar’s automatic 1% APR inflation to our users, keeping none of the weekly airdrops for ourselves.</p>
+            <h3 class="text-info">Fast and cheap</h3>
+            <p>Transactions settle in seconds and you only have to pay the Stellar network transaction fee. No transaction fee has to be payed to Lumenshine.</p>
+          </b-col>
+          <b-col cols="12" class="pt-5 my-5">
+            <h3 class="text-info">Available for web and soon for mobile</h3>
+            <p>Lumenshine is currently available for web and we will soon release our mobile apps for iOS and Android soon.</p>
+          </b-col>
+          <b-col cols="12" class="pt-5 my-5">
+            <h3 class="text-info">Beta version</h3>
+            <p>This version of Lumenshine is currently beta.</p>
+          </b-col>
+          <b-col cols="12" class="pt-5 my-5">
+            <h3 class="text-info">Stellar network</h3>
+            <p>The Stellar network is hosted by numerous distributed nodes world-wide who are not under the control of any single entity or groups of entities. Lumenshine is only an interface for the Stellar network and does not control the Stellar network.</p>
           </b-col>
         </b-row>
       </b-card>
