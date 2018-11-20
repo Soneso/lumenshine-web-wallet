8ed5a17990d74954cd24872ee585d28887ab1b68
Cristian Maghiar <cristi@avangard-media.ro>
2018-11-20 19:39:05 +0200

    better CTA

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,16 +3,21 @@
     <b-col cols="12" sm="11" md="10" lg="9" xl="8">
       <b-card class="p-4">
         <b-row>
-          <b-col cols="12" sm="6" md="5" lg="4" xlg="3">
+          <b-col cols="12" lg="4" xlg="3">
             <h1><small class="text-info">Introducing</small> <span class="font-weight-800 text-primary">Lumenshine <small>(beta)</small></span></h1>
-            <p class="lead">Lumenshine is a beautiful, fully-featured Wallet for the public Stellar network. It is developed and maintained by Soneso GmbH, Germany. <router-link to="/register">Sign-Up</router-link> and interact with the Stellar network with ease.</p>
+            <p class="lead">Lumenshine is a beautiful, fully-featured Wallet for the public Stellar network. It is developed and maintained by Soneso GmbH, Germany.</p>
           </b-col>
           <b-col>
             <img src="/static/app-screenshot.png">
           </b-col>
-          <b-col cols="12" class="py-5 text-center">
-            <b-button href="/register" size="lg" variant="outline-success" class="btn-rounded text-uppercase mx-3">sign up</b-button>
-            <b-button href="/login" size="lg" variant="outline-info" class="btn-rounded text-uppercase mx-3">log in</b-button>
+          <b-col cols="12" class="py-3 text-center">
+            <hr class="divider light">
+            <div class="py-3">
+              <div class="lead pb-4">Sign up and easily interact with the Stellar network!</div>
+              <b-button href="/register" size="lg" variant="outline-success" class="btn-rounded text-uppercase mx-3 mb-2">sign up</b-button>
+              <b-button href="/login" size="lg" variant="outline-info" class="btn-rounded text-uppercase mx-3 mb-2">log in</b-button>
+            </div>
+            <hr class="divider light">
           </b-col>
           <b-col cols="12" class="pt-3">
             <h2 class="text-info">Fully-featured and beautiful</h2>
