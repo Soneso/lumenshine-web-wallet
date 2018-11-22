0f9906f5b996de404089967b063a3a8428760f87
Dani Biró <danibiro93@gmail.com>
2018-11-22 09:50:22 +0200

    Add new backend configs.

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,8 @@ export default {
   // do not forget authToken on page reload (for development)
   KEEP_LOGGED_IN: process.env.NODE_ENV === 'development',
 
-  ...((__BUILD_CONFIG__ === 'demo') ? require('./demo').default : require('./alpha').default)
+  ...((__BUILD_CONFIG__ === 'demo') ? require('./demo').default : {}),
+  ...((__BUILD_CONFIG__ === 'alpha') ? require('./alpha').default : {}),
+  ...((__BUILD_CONFIG__ === 'lsdev') ? require('./lsdev').default : {}),
+  ...((__BUILD_CONFIG__ === 'lsstage') ? require('./lsstage').default : {}),
 };
---
+++ src/config/lsstage.js
@@ -0,0 +1,35 @@
+/* global __NODE_SCRIPT__ */
+const nodeScript = __NODE_SCRIPT__;
+
+export default {
+  APP_TITLE: 'Lumenshine',
+  APP_SUBTITLE: 'A Wallet for the Stellar Network by Soneso',
+
+  REGISTRATION_OPTIONAL_FIELDS: [
+    'forename',
+    'lastname',
+    // 'companyName',
+    // 'salutation',
+    // 'title',
+    // 'streetAddress',
+    // 'streetNumber',
+    // 'zipCode',
+    // 'city',
+    // 'state',
+    // 'country',
+    // 'nationality',
+    // 'mobilePhone',
+    // 'birthday',
+    // 'birthPlace'
+  ],
+
+  SUPPORT_MAIL: 'support@lumenshine.com',
+  FEDERATION_DOMAIN: 'alpha.lumenshine.com',
+
+  API_BASE: nodeScript === 'dev-local' ? 'http://localhost:9001' : 'https://lsstageapi.ponytest.de',
+
+  HORIZON_URL: 'https://lsstagehorizon.ponytest.de',
+  IS_TEST_NETWORK: false,
+
+  SERVER_PUBLIC_KEY: 'GBGXAY3HDXMUWAUDATBZ5SVGLFUC5GKJC4BNN5MEPVLWKCOMBXQUIUWM',
+};
