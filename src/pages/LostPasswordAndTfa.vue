99cb535b7691a70580dea3331888401fd41c14ab
Cristian Maghiar <cristi@avangard-media.ro>
2018-11-22 19:03:26 +0200

    lost 2fa and password, new text

--- src/pages/LostPasswordAndTfa.vue
+++ src/pages/LostPasswordAndTfa.vue
@@ -3,7 +3,7 @@
     <b-col cols="11" sm="9" md="7" lg="6" xl="5">
       <b-card class="p-4 single-card text-center">
         <h4 class="form-headline text-danger pb-3">Lost Password & 2FA Secret</h4>
-        <p>We can not recover your account automatically, please contact</p>
+        <p>We cannot recover your account automatically; please send us an email by clicking the link below and attach a photo of the front and backside of your ID. We will reset your 2fa secret manually and will inform you about the next steps. Then you will need your mnemonic backup secret to be able to continue the recovering process.</p>
         <p><a :href="`mailto:${config.SUPPORT_MAIL}?subject=${encodeURIComponent('Lost Password & 2FA Secret')}`">support</a></p>
       </b-card>
     </b-col>
