f86075456a034d6dede6b07f2fbf5fb6b1d7b59d
Dani Biró <danibiro93@gmail.com>
2018-11-27 11:44:34 +0200

    Deposit screen

--- src/components/cards/WalletCard.vue
+++ src/components/cards/WalletCard.vue
@@ -20,11 +20,14 @@
         <b-col>
           <template v-if="!data.stellar_data">
             <div class="mx-2 py-2 text-right">
-              <a href="#" class="p-1" @click.prevent="fundWalletModalVisible = true">Fund Wallet</a>
+              <a href="#" class="p-1" @click.prevent="depositModalVisible = true">Fund Wallet</a>
             </div>
           </template>
 
           <template v-else-if="showActions">
+            <div v-if="!config.IS_TEST_NETWORK" class="mx-2 py-2 float-left">
+              <a href="#" class="px-1" @click.prevent="depositModalVisible = true">Deposit</a>
+            </div>
             <div class="mx-2 py-2 text-right">
               <a href="#" class="px-1" @click.prevent="resetSendPayment(), sendModalVisible = true">Send</a>
               <a href="#" class="px-1" @click.prevent="receiveModalVisible = true">Receive</a>
@@ -69,47 +72,14 @@
         @close="onDetailsClose"/>
     </b-modal>
 
-    <b-modal v-model="fundWalletModalVisible" :title="!config.IS_TEST_NETWORK ? 'Fund Wallet' : 'Fund Wallet via Friendbot'" size="md" hide-footer>
-      <div v-if="!config.IS_TEST_NETWORK" class="text-center">
-        <div class="py-3">
-          <qrcode :options="{ width: 200, margin: 0, color: { light: '#F4F6F2', dark: '#000000' } }" :value="data.public_key" class="bar-code-img"/>
-        </div>
-        <div class="font-weight-600">Account ID / Public key</div>
-        <div class="break-word with-hyphens my-3">
-          <public-key :text="data.public_key"/>
-        </div>
-      </div>
-      <div v-else class="text-center"> <!-- Test network -->
-        <p class="font-weight-600">Stellar test net public key</p>
-        <div class="break-word with-hyphens my-1">
-          <public-key :text="data.public_key"/>
-        </div>
-      </div>
-
-      <div v-if="!config.IS_TEST_NETWORK">
-        <p>In order to prevent people from making a huge number of unnecessary accounts, each account in the stellar blockchain must have a minimum balance of 1 XLM (Stellar Lumen). Please send your Stellar Lumens (XLM) to the above displayed Account ID / Public key. At least 1 XLM is needed to fund your wallet in the stellar blockchain. We recommend a minimum of 2 XLM.</p>
-        <p>Q: I don't have Stellar Lumens. Where can I get Stellar Lumens (XLM)?</p>
-        <p> A: You can pay an exchange that sells lumens in order to fund your wallet. <a href="http://coinmarketcap.com/currencies/stellar/#markets" target="_blank">CoinMarketCap</a> maintains a list of exchanges that sell Stellar Lumens (XML). After purchasing the lumens withdraw them from the exchange to your wallet by sending them to the above displayed Account ID / Public key in order to fund your wallet.</p>
-        <!-- <a id="changellyButton" :href="`https://changelly.com/widget/v1?auth=email&from=USD&to=XLM&merchant_id=dcaa3ae0e64f&address=${data.public_key}&amount=100&ref_id=dcaa3ae0e64f&color=00cf70`" target="_blank" @click.prevent="changellyModalVisible = true, fundWalletModalVisible = false">
-          <img src="https://changelly.com/pay_button_pay_with.png">
-        </a> -->
-      </div>
-      <div v-else class="text-center pt-3">
-        <small>This client operates on the test net. Do not send real Stellar Lumens from the main/public net. To fund your wallet for testing purposes we can kindly ask Friendbot to send you some test lumens. Please press the button below to receive the test net lumens from Freindbot.</small>
-        <br>
-        <b-button v-if="!fundWalletLoading" variant="primary" class="btn-rounded my-3" @click.prevent="fundWithFriendbot">
-          Fund with test lumens
-        </b-button>
-        <b-button v-else variant="primary" class="btn-rounded my-3">
-          <spinner :size="24" variant="white" message="funding..." width="90"/>
-        </b-button>
-      </div>
+    <b-modal v-model="depositModalVisible" title="Deposit" size="md" hide-footer>
+      <wallet-deposits v-if="depositModalVisible" :data="data" @close="depositModalVisible = false" @changelly="onShowChangelly"/>
     </b-modal>
 
     <div v-if="!config.IS_TEST_NETWORK" id="changellyModal" :style="{display: changellyModalVisible ? 'block' : 'none'}">
       <div class="changellyModal-content">
         <span class="changellyModal-close" @click="changellyModalVisible = false">x</span>
-        <iframe :src="`https://changelly.com/widget/v1?auth=email&from=USD&to=XLM&merchant_id=dcaa3ae0e64f&address=${data.public_key}&amount=100&ref_id=dcaa3ae0e64f&color=00cf70`" width="600" height="500" class="changelly" scrolling="no" style="overflow-y: hidden; border: none">
+        <iframe :src="`https://changelly.com/widget/v1?auth=email&from=${changellyCurrency}&to=XLM&merchant_id=dcaa3ae0e64f&address=${data.public_key}&amount=100&ref_id=dcaa3ae0e64f&color=00cf70`" width="600" height="500" class="changelly" scrolling="no" style="overflow-y: hidden; border: none">
           Can't load widget
         </iframe>
       </div>
@@ -130,6 +100,7 @@ import WalletSecretSeedForm from '@/components/forms/wallets/WalletSecretSeedFor
 
 import WalletCardDetails from '@/components/cards/WalletCardDetails';
 import WalletCardBalances from '@/components/cards/WalletCardBalances';
+import WalletDeposits from '@/components/cards/WalletDeposits';
 
 import spinner from '@/components/ui/spinner';
 import publicKey from '@/components/ui/publicKey';
@@ -141,6 +112,7 @@ export default {
     WalletSecretSeedForm,
     WalletCardDetails,
     WalletCardBalances,
+    WalletDeposits,
     spinner,
     publicKey
   },
@@ -165,10 +137,12 @@ export default {
       fundWalletLoading: false,
 
       changellyModalVisible: false,
-      fundWalletModalVisible: false,
+      depositModalVisible: false,
       receiveModalVisible: false,
       sendModalVisible: false,
       detailsModalVisible: false,
+
+      changellyCurrency: 'USD',
 
       config
     };
@@ -215,10 +189,18 @@ export default {
       'updateWallets',
       'resetDecryptedWallet'
     ]),
+
+    onShowChangelly (currency) {
+      this.changellyCurrency = currency;
+      this.depositModalVisible = false;
+      this.changellyModalVisible = true;
+    },
+
     onCopy () {
       this.accountIDCopied = true;
       setTimeout(() => { this.accountIDCopied = false; }, 2e3);
     },
+
     async onSendPaymentClick (data) {
       await this.sendPayment(data);
     },
@@ -298,7 +280,7 @@ export default {
       this.fundWalletLoading = true;
       await this.fundAccountWithFriendbot(this.data.public_key);
       this.fundWalletLoading = false;
-      this.fundWalletModalVisible = false;
+      this.depositModalVisible = false;
     },
     onDetailsClose (next) {
       this.detailsModalVisible = false;
---
+++ src/components/cards/WalletDeposits.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="p-4">
+    <div v-if="!config.IS_TEST_NETWORK"> <!-- Live net -->
+      <div v-if="step === 'main'">
+        <h4 class="font-weight-700 mb-3">WHAT WOULD YOU LIKE TO DEPOSIT?</h4>
+        <p>Lumenshine accepts USD fiat deposits and all types of crypto. Choose what funds you’d like to add to your account.</p>
+        <!-- <b-button variant="info" class="btn-rounded my-3" @click.prevent="step = 'usd'">USD</b-button> -->
+        <b-button variant="info" class="btn-rounded my-3" @click.prevent="step = 'xlm'">XLM</b-button>
+        <b-button variant="info" class="btn-rounded my-3" @click.prevent="step = 'other'">Other crypto</b-button>
+      </div>
+
+      <!-- <div v-else-if="step === 'usd'">
+        <p class="font-weight-700">USD</p>
+        <p>Lumenshine uses Changelly as our cross-chain exchange provider. They convert your funds to XLM at the best rates by bidding out your order worldwide. They charge 0.5% for the conversion. Lumenshine takes no fee.</p>
+        <a href="#" target="_blank" class="changelly-button" @click.prevent="$emit('changelly', 'USD')">
+          <img src="@/assets/images/ui/pay-with-changelly.png">
+        </a>
+      </div> -->
+
+      <div v-else-if="step === 'xlm'">
+        <h4 class="font-weight-700 mb-3">XLM</h4>
+        <p>Funding with XLM is free and only takes a few seconds to confirm.</p>
+        <b-card class="flat-card">
+          <b-row>
+            <b-col>
+              <div class="break-word with-hyphens my-4">
+                <public-key :text="data.public_key"/>
+              </div>
+            </b-col>
+            <b-col cols="3" class="text-right">
+              <qrcode :options="{ width: 88, margin: 0 }" :value="data.public_key" class="bar-code-img"/>
+            </b-col>
+          </b-row>
+        </b-card>
+        <span class="mx-1"><b-button variant="success" class="btn-rounded my-3" @click.prevent="$emit('close')">OK, done</b-button></span>
+        <span class="mx-1"><b-button variant="info" class="btn-rounded my-3" @click.prevent="step = 'main'">Make another deposit</b-button></span>
+      </div>
+      <div v-else-if="step === 'other'">
+        <h4 class="font-weight-700 mb-3">Other Crypto</h4>
+        <p>Lumenshine uses Changelly as our cross-chain exchange provider. They convert your funds to XLM at the best rates by bidding out your order worldwide. They charge 0.5% for the conversion.</p>
+
+        <a href="#" target="_blank" class="changelly-button" @click.prevent="$emit('changelly', 'BTC')">
+          <img src="@/assets/images/ui/pay-with-changelly.png">
+        </a>
+      </div>
+    </div>
+
+    <div v-if="config.IS_TEST_NETWORK" class="pt-3">
+      <p class="font-weight-700">Stellar test net public key</p>
+      <div class="break-word with-hyphens my-1">
+        <public-key :text="data.public_key"/>
+      </div>
+      <small>This client operates on the test net. Do not send real Stellar Lumens from the main/public net. To fund your wallet for testing purposes we can kindly ask Friendbot to send you some test lumens. Please press the button below to receive the test net lumens from Freindbot.</small>
+      <br>
+      <b-button v-if="!fundWalletLoading" variant="info" class="btn-rounded my-3" @click.prevent="fundWithFriendbot">
+        Fund with test lumens
+      </b-button>
+      <b-button v-else variant="info" class="btn-rounded my-3">
+        <spinner :size="24" variant="white" message="funding..." width="90"/>
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+import config from '@/config';
+
+import spinner from '@/components/ui/spinner';
+import publicKey from '@/components/ui/publicKey';
+
+export default {
+  name: 'WalletDeposits',
+  components: { spinner, publicKey },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data () {
+    return {
+      config,
+
+      step: 'main',
+
+      fundWalletLoading: false,
+      fundWalletModalVisible: true,
+    };
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    ...mapActions([
+      'fundAccountWithFriendbot',
+    ]),
+    async fundWithFriendbot () {
+      this.fundWalletLoading = true;
+      await this.fundAccountWithFriendbot(this.data.public_key);
+      this.fundWalletLoading = false;
+      this.fundWalletModalVisible = false;
+    },
+  }
+};
+</script>
