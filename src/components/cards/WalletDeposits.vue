f111b51894cd32f19b877a6bad0794853f5d28bd
Dani Biró <danibiro93@gmail.com>
2018-11-28 13:03:18 +0200

    Use AnchorUSD

--- src/components/cards/WalletDeposits.vue
+++ src/components/cards/WalletDeposits.vue
@@ -4,18 +4,19 @@
       <div v-if="step === 'main'">
         <h4 class="font-weight-700 mb-3">WHAT WOULD YOU LIKE TO DEPOSIT?</h4>
         <p>Lumenshine accepts USD fiat deposits and all types of crypto. Choose what funds you’d like to add to your account.</p>
-        <!-- <b-button variant="info" class="btn-rounded my-3" @click.prevent="step = 'usd'">USD</b-button> -->
+        <b-button variant="info" class="btn-rounded my-3" @click.prevent="step = 'usd'">USD</b-button>
         <b-button variant="info" class="btn-rounded my-3" @click.prevent="step = 'xlm'">XLM</b-button>
         <b-button variant="outline-info" class="btn-rounded my-3" @click.prevent="step = 'other'">Other crypto</b-button>
       </div>
 
-      <!-- <div v-else-if="step === 'usd'">
-        <p class="font-weight-700">USD</p>
-        <p>Lumenshine uses Changelly as our cross-chain exchange provider. They convert your funds to XLM at the best rates by bidding out your order worldwide. They charge 0.5% for the conversion. Lumenshine takes no fee.</p>
-        <a href="#" target="_blank" class="changelly-button" @click.prevent="$emit('changelly', 'USD')">
+      <div v-else-if="step === 'usd'">
+        <p class="font-weight-700">Deposit with ANCHORUSD</p>
+        <p>You can deposits US dollar via <a href="https://www.anchorusd.com/" target="_blank" rel="noopener">AnchorUSD</a>, the first platform on Stellar to offer the convenience of direct ACH debit. You’ll need to submit some basic identity information to them and their banking partner, which takes just a few seconds.</p>
+        <p>Then you can connect your bank and begin depositing USD directly to your Stellar account. The minimum cash deposit is 1000 USD, and AnchorUSD charges a flat 25 USD fee for the service.</p>
+        <!-- <a href="#" target="_blank" class="changelly-button" @click.prevent="$emit('changelly', 'USD')">
           <img src="@/assets/images/ui/pay-with-changelly.png">
-        </a>
-      </div> -->
+        </a> -->
+      </div>
 
       <div v-else-if="step === 'xlm'">
         <h4 class="font-weight-700 mb-3">XLM</h4>
