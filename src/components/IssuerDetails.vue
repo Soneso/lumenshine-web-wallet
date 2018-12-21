2ec6b38eb05161fc07278ad7fe9ee52e1f81383b
Christian Rogobete <c.rogobete@soneso.com>
2018-12-21 23:53:20 +0100

    add minor improvements & bug fixes to currency details

--- src/components/IssuerDetails.vue
+++ src/components/IssuerDetails.vue
@@ -1,37 +1,34 @@
 <template>
   <div>
     <br>
-    <h5>Currency details</h5>
-    Currency: {{ assetCode }}<br>
-    Issuer public key: <public-key :text="issuer" color="text-secondary"/><br>
-    Home domain: {{ homeDomain }}<br>
+    <h5>Metadata provided by the issuer</h5>
     <template v-if="currencyDetails">
-      <span v-if="currencyDetails.name">Name: {{ currencyDetails.name }}<br></span>
-      <span v-if="currencyDetails.desc">Description: {{ currencyDetails.desc }}<br></span>
-      <span v-if="currencyDetails.conditions">Conditions: {{ currencyDetails.conditions }}<br></span>
-      <span v-if="currencyDetails.image">Image: <img :src="currencyDetails.image" style="max-width: 60px"><br></span>
-      <span v-if="currencyDetails.code">Code: {{ currencyDetails.code }}<br></span>
-      <span v-if="currencyDetails.code_template">Code template: {{ currencyDetails.code_template }}<br></span>
-      <span v-if="currencyDetails.issuer">Issuer: {{ currencyDetails.issuer }}<br></span>
       <span v-if="currencyDetails.status">Status: {{ currencyDetails.status }}<br></span>
-      <span v-if="currencyDetails.display_decimals">Number of decimals to show: {{ currencyDetails.display_decimals }}<br></span>
+      <span v-if="currencyDetails.name">Short name: {{ currencyDetails.name }}<br></span>
+      <span v-if="currencyDetails.desc">Description: {{ currencyDetails.desc }}<br></span>
+      <span v-if="currencyDetails.display_decimals">Display digits: {{ currencyDetails.display_decimals }}<br></span>
+      <span v-if="currencyDetails.conditions">Conditions on token: {{ currencyDetails.conditions }}<br></span>
+      <span v-if="currencyDetails.image">Image:<br><br><img :src="currencyDetails.image" style="max-width: 60px"><br><br></span>
+      <span v-if="currencyDetails.code">Code: {{ currencyDetails.code }}<br></span>
+      <!--<span v-if="currencyDetails.code_template">Code template: {{ currencyDetails.code_template }}<br></span>-->
+      <span v-if="currencyDetails.issuer">Issuer: {{ currencyDetails.issuer }}<br></span>
       <span v-if="currencyDetails.fixed_number">Fixes number of tokens: {{ currencyDetails.fixed_number }}<br></span>
       <span v-if="currencyDetails.max_number">Max number of tokens: {{ currencyDetails.max_number }}<br></span>
-      <span v-if="currencyDetails.is_unlimited !== undefined">Is unlimited: {{ currencyDetails.is_unlimited ? 'true' : 'false' }}<br></span>
-      <span v-if="currencyDetails.is_asset_anchored !== undefined">Is asset anchored: {{ currencyDetails.is_asset_anchored ? 'true' : 'false' }}<br></span>
+      <span v-if="currencyDetails.is_unlimited !== undefined">Unlimited: {{ currencyDetails.is_unlimited ? 'yes' : 'no' }}<br></span>
+      <span v-if="currencyDetails.is_asset_anchored !== undefined">Anchored: {{ currencyDetails.is_asset_anchored ? 'yes' : 'no' }}<br></span>
       <span v-if="currencyDetails.anchor_asset_type">Type of asset anchored: {{ currencyDetails.anchor_asset_type }}<br></span>
       <span v-if="currencyDetails.anchor_asset">Anchor asset: {{ currencyDetails.anchor_asset }}<br></span>
       <span v-if="currencyDetails.redemption_instructions">Redemption instructions: {{ currencyDetails.redemption_instructions }}<br></span>
-      <span v-if="currencyDetails.collateral_addresses">Collateral addresses: <span v-for="addr in currencyDetails.collateral_addresses" :key="addr">{{ addr }} </span><br></span>
-      <span v-if="currencyDetails.collateral_address_messages">Collateral address messages: <span v-for="msg in currencyDetails.collateral_address_messages" :key="msg">{{ msg }} </span><br></span>
-      <span v-if="currencyDetails.collateral_address_messages">Collateral address signitures: <span v-for="signiture in currencyDetails.collateral_address_messages" :key="signiture">{{ signiture }} </span><br></span>
-      <span v-if="currencyDetails.regulated !== undefined">Regulated: {{ currencyDetails.regulated ? 'true' : 'false' }}<br></span>
+      <span v-if="currencyDetails.collateral_addresses"><br>Collateral addresses:<br><span v-for="addr in currencyDetails.collateral_addresses" :key="addr">{{ addr }}<br></span><br></span>
+      <span v-if="currencyDetails.collateral_address_messages">Collateral address messages:<br><span v-for="msg in currencyDetails.collateral_address_messages" :key="msg">{{ msg }}<br></span><br></span>
+      <span v-if="currencyDetails.collateral_address_signatures">Collateral address signitures:<br><span v-for="signiture in currencyDetails.collateral_address_signatures" :key="signiture">{{ signiture }}<br></span><br></span>
+      <span v-if="currencyDetails.regulated !== undefined">Regulated: {{ currencyDetails.regulated ? 'yes' : 'no' }}<br></span>
       <span v-if="currencyDetails.approval_server">Approval server: {{ currencyDetails.approval_server }}<br></span>
       <span v-if="currencyDetails.approval_criteria">Approval criteria: {{ currencyDetails.approval_criteria }}<br></span>
     </template>
 
     <br>
-    <h6>Organisation details</h6>
+    <h6>Documentation</h6>
 
     Name: {{ details.DOCUMENTATION.ORG_NAME || 'not found' }}<br>
 
@@ -42,12 +39,12 @@
     <template v-else>not found</template>
     <br>
 
-    <span v-if="details.DOCUMENTATION.ORG_LOGO">Logo: <img :src="details.DOCUMENTATION.ORG_LOGO" style="max-width: 60px"><br></span>
+    <span v-if="details.DOCUMENTATION.ORG_LOGO">Logo:<br><br><img :src="details.DOCUMENTATION.ORG_LOGO" style="max-width: 60px"><br><br></span>
 
     Description: {{ details.DOCUMENTATION.ORG_DESCRIPTION || 'not found' }}<br>
 
-    Official Email address:
-    <a v-if="details.DOCUMENTATION.ORG_TWITTER" :href="`mailto:${details.DOCUMENTATION.ORG_OFFICIAL_EMAIL}`" target="_blank" rel="noopener noreferer">{{ details.DOCUMENTATION.ORG_OFFICIAL_EMAIL }} <span v-if="!details.DOCUMENTATION.ORG_OFFICIAL_EMAIL.endsWith('@' + homeDomain)" class="text-danger">(incompatible domain)</span></a>
+    Official email:
+    <a v-if="details.DOCUMENTATION.ORG_TWITTER" :href="`mailto:${details.DOCUMENTATION.ORG_OFFICIAL_EMAIL}`" target="_blank" rel="noopener noreferer">{{ details.DOCUMENTATION.ORG_OFFICIAL_EMAIL }} <!-- not home domain, compare to org url hostname: <span v-if="!details.DOCUMENTATION.ORG_OFFICIAL_EMAIL.endsWith('@' + homeDomain)" class="text-danger">(incompatible domain)</span>--></a>
     <template v-else>not found</template>
     <br>
 
@@ -57,7 +54,7 @@
     <a v-if="details.DOCUMENTATION.ORG_PHYSICAL_ADDRESS_ATTESTATION" :href="details.DOCUMENTATION.ORG_PHYSICAL_ADDRESS_ATTESTATION" target="_blank" rel="noopener noreferer">
       {{ details.DOCUMENTATION.ORG_PHYSICAL_ADDRESS_ATTESTATION }}
       <span v-if="!details.DOCUMENTATION.ORG_PHYSICAL_ADDRESS_ATTESTATION.startsWith('https://')" class="text-danger">(not secure) </span>
-      <span v-if="!details.DOCUMENTATION.ORG_PHYSICAL_ADDRESS_ATTESTATION.endsWith('@' + homeDomain)" class="text-danger">(incompatible domain)</span>
+      <!-- not home domain, compare to org url hostname:<span v-if="!details.DOCUMENTATION.ORG_PHYSICAL_ADDRESS_ATTESTATION.endsWith('@' + homeDomain)" class="text-danger">(incompatible domain)</span>-->
     </a>
     <template v-else>not found</template>
     <br>
@@ -68,7 +65,7 @@
     <a v-if="details.DOCUMENTATION.ORG_PHONE_NUMBER_ATTESTATION" :href="details.DOCUMENTATION.ORG_PHONE_NUMBER_ATTESTATION" target="_blank" rel="noopener noreferer">
       {{ details.DOCUMENTATION.ORG_PHONE_NUMBER_ATTESTATION }}
       <span v-if="!details.DOCUMENTATION.ORG_PHONE_NUMBER_ATTESTATION.startsWith('https://')" class="text-danger">(not secure) </span>
-      <span v-if="!details.DOCUMENTATION.ORG_PHONE_NUMBER_ATTESTATION.endsWith('@' + homeDomain)" class="text-danger">(incompatible domain)</span>
+      <!-- not home domain, compare to org url hostname: <span v-if="!details.DOCUMENTATION.ORG_PHONE_NUMBER_ATTESTATION.endsWith('@' + homeDomain)" class="text-danger">(incompatible domain)</span>-->
     </a>
     <template v-else>not found</template>
     <br>
@@ -98,10 +95,13 @@
         <li v-for="principal in details.PRINCIPALS" :key="principal.name + principal.email">
           <span v-if="principal.name">Name: {{ principal.name }}<br></span>
           <span v-if="principal.email">Email: <a :href="`mailto:${principal.emailL}`" target="_blank" rel="noopener noreferer">{{ principal.email }}</a><br></span>
-          <span v-if="principal.github">Github: <a :href="`https://github.com/${principal.github}`" target="_blank" rel="noopener noreferer">{{ principal.github }}}</a><br></span>
-          <span v-if="principal.keybase">Keybase: {{ principal.keybase }}<br></span>
-          <span v-if="principal.telegram">Telegram: {{ principal.telegram }}<br></span>
-          <span v-if="principal.twitter">Twitter: <a :href="`https://twitter.com/${principal.twitter}`" target="_blank" rel="noopener noreferer">{{ principal.twitter }}</a><br></span>
+          <span v-if="principal.github">Github: <a :href="`https://github.com/${principal.github}`" target="_blank" rel="noopener noreferer">{{ principal.github }}</a><br></span>
+          <span v-if="principal.keybase">Keybase: <a :href="`https://keybase.io/${principal.keybase}`" target="_blank" rel="noopener noreferer">{{ principal.keybase }}</a><br></span>
+          <span v-if="principal.telegram">Telegram: <a :href="`https://t.me/${principal.telegram}`" target="_blank" rel="noopener noreferer">{{ principal.telegram }}</a><br></span>
+          <span v-if="principal.twitter">Twitter: <a :href="`https://twitter.com/${principal.twitter }`" target="_blank" rel="noopener noreferer">{{ principal.twitter }}</a><br></span>
+          <span v-if="principal.id_photo_hash">ID photo hash: {{ principal.id_photo_hash }}<br></span>
+          <span v-if="principal.verification_photo_hash">Verification photo hash: {{ principal.verification_photo_hash }}<br></span>
+          <br>
         </li>
       </ul>
     </template>
